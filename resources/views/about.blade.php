f018fbdeec3ac6192a3824a726d8bff40825ac5a
Dnurudeen <danijunurdeen@gmail.com>
2024-06-14 17:04:13 +0100

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.master')
+@section('title', 'About Us')
+
+@section('content')
+    <div class="contaniner">
+        <div class="row">
+            <div class="col-sm-6 mb-3 mb-sm-0">
+            <div class="card">
+                <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            </div>
+            <div class="col-sm-6">
+            <div class="card">
+                <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+{{-- END --}}
